a83a65c249797ac41374000169065f71e9b13da3
rachel455 <rachelishak@gmail.com>
2021-11-15 16:04:05 -0500

    updated LogIn.js and added buttons

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,6 +1,9 @@
 // Login.js
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
+import AccountBalance from './AccountBalance';
+import {Link} from 'react-router-dom';
+import './LogIn.css';
 
 class LogIn extends Component {
   constructor () {
@@ -47,6 +50,33 @@ class LogIn extends Component {
           </div>
           <button>Log In</button>
         </form>
+        <body>
+          <a>
+            <Link to="/UserProfile">
+              <button type="button">User Profile</button>
+            </Link>
+          </a>
+          <a>
+            <Link to="/LogIn">
+              <button type="button">Log In</button>
+            </Link>
+          </a>
+          <a>
+            <Link to="/Credits">
+              <button type= "button">Credits</button>
+            </Link>
+          </a>
+          <a>
+            <Link to="/Debits">
+              <button type= "button">Debits</button>
+            </Link>
+          </a>
+        </body>
+          <h2>Account Balance</h2>
+          <AccountBalance accountBalance={this.props.accountBalance}/>
+          <Link to="/">
+              <button type= "button">Return to Home</button>
+          </Link>
       </div>
     )
   }
