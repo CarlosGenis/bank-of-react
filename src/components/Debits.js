2f1cbbcaf168c940d9453db7829747419e73afcf
rachel455 <rachelishak@gmail.com>
2021-11-15 01:01:51 -0500

    added import from AccountBalance.js and add the balance at the bottom of the page

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -1,47 +1,30 @@
-// src/components/debits.js
+// src/components/Debits.js
+
 import React, {Component} from 'react';
-import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
-import axios from 'axios';
- class Debits extends Component{
-     constructor (props) {
-         super(props) 
-         this.state = {
-             debits: []
-         }
-     }
-     componentDidMount() {
-         axios.get('https://moj-api.herokuapp.com/debits')
-             .then(response => this.setState({
-                 debits: response.data }));
-     }
+import AccountBalance from './AccountBalance'
 
-     addDebit(enteredAmount) {
-         <AccountBalance accountBalance={this.props.accountBalance + enteredAmount}/>
-     }
-
-     render() {
-      let debitsView = () => {
-          return this.props.debits.map((debit) => {
-              let date = debit.date.slice(0,10);
-              return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
-          })
-      }
-  
-      return (
-      <div>
-        <h1>Debits</h1>
-        {debitsView()}
-           <form onSubmit={this.props.addDebit}>
+const Debits = (props) => {
+	let debitsView = () => {
+        const { debits } = props;
+        return debits.map((debit) => {
+            let date = debit.date.slice(0,10);
+            return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
+        })
+    }
+    return (
+    	<div>
+    	   <h1>Debits</h1>
+    	   {debitsView()}
+           <form onSubmit={props.addDebit}>
              <input type="text" name="description" />
              <input type="number" name="amount" />
              <button type="submit">Add Debit</button>
            </form>
-  
-              <AccountBalance accountBalance={this.props.accountBalance}/>
-          </div>
-      );
-    }  
- }
+           <h1>Account Balance:</h1>
+					 <AccountBalance accountBalance={props.accountBalance}/>
+    	</div>
 
+    )
+}
 export default Debits;
