5ed178f5f7811cc668902a5652c296f060ea0041
rachel455 <rachelishak@gmail.com>
2021-11-15 00:35:58 -0500

    added ComponentDidMount(), addDebit(), render() to Debit.js

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -1,28 +1,47 @@
-// src/components/Debits.js
-
+// src/components/debits.js
 import React, {Component} from 'react';
+import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+ class Debits extends Component{
+     constructor (props) {
+         super(props) 
+         this.state = {
+             debits: []
+         }
+     }
+     componentDidMount() {
+         axios.get('https://moj-api.herokuapp.com/debits')
+             .then(response => this.setState({
+                 debits: response.data }));
+     }
 
-const Debits = (props) => {
-	let debitsView = () => {
-        const { debits } = props;
-        return debits.map((debit) => {
-            let date = debit.date.slice(0,10);
-            return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
-        })
-    }
-    return (
-    	<div>
-    	   <h1>Debits</h1>
-    	   {debitsView()}
-           <form onSubmit={props.addDebit}>
+     addDebit(enteredAmount) {
+         <AccountBalance accountBalance={this.props.accountBalance + enteredAmount}/>
+     }
+
+     render() {
+      let debitsView = () => {
+          return this.props.debits.map((debit) => {
+              let date = debit.date.slice(0,10);
+              return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
+          })
+      }
+  
+      return (
+      <div>
+        <h1>Debits</h1>
+        {debitsView()}
+           <form onSubmit={this.props.addDebit}>
              <input type="text" name="description" />
              <input type="number" name="amount" />
              <button type="submit">Add Debit</button>
            </form>
-					 <h1>Account Balance:</h1>
-    	</div>
+  
+              <AccountBalance accountBalance={this.props.accountBalance}/>
+          </div>
+      );
+    }  
+ }
 
-    )
-}
 export default Debits;
